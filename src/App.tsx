87f11114fb8d2255e5566190e47791c0c84d47a9
launchpencil <launchpencil@gmail.com>
2026-04-14 14:11:13 +0900

    表示周りをいい感じに

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
+import timedata from './assets/time.json'
 import sample from './assets/sample.json'
 
 interface Period {
-  period: string;
+  name: string;
   cells: (ClassCell | OthersCell)[];
 }
 
@@ -32,6 +33,11 @@ function Cell({ data }: { data: ClassCell | OthersCell }) {
   }
 }
 
+function Time({ name }: { name: string }) {
+  const timeData = timedata.find((t) => t.period === name);
+  return <div className="time">{timeData?.time}</div>;
+}
+
 function App() {
   //var time = localStorage.getItem('time')
   var time = sample as Period[]
@@ -50,9 +56,16 @@ function App() {
             <div className="header">水</div>
             <div className="header">木</div>
             <div className="header">金</div>
-            {time.map((period) => (              period.cells.map((cell) => (
-                <Cell data={cell} />
-              ))
+            {time.map((period) => (
+              
+              <>
+                <div className="period">{period.name}
+                  <Time name={period.name} />
+                </div>
+                {period.cells.map((cell) => (
+                  <Cell data={cell} />
+                ))}
+              </>
             ))}
           </div>
         </div>
