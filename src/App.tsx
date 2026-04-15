c0106a789b53a81f6db1500f2b207d27e3215452
launchpencil <launchpencil@gmail.com>
2026-04-15 16:16:18 +0900

    コード周りを分けていい感じに

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,18 @@
 import './App.css'
-import timedata from './assets/time.json'
-import sample from './assets/sample.json'
 
-interface Period {
-  name: string;
-  cells: (ClassCell | OthersCell)[];
-}
-
-interface OthersCell {
-  type: "same" | "empty"
-  raw: "上記と同じ" | "空き"
-}
-
-interface ClassCell {
-  type: "class";
-  subject: string;
-  teacher: string;
-  room?: string;
-};
-
-function Cell({ data }: { data: ClassCell | OthersCell }) {
-  switch (data.type) {
-    case 'class':
-      return (
-        <div className="cell">
-          <div className='class'>
-            <div className="class-name">{data.subject}</div>
-            <div className="teacher">{data.teacher}</div>
-            <div className="room">{data.room}</div>
-          </div>
-        </div>
-      )
-    case 'same':
-      return (
-        <div className={`cell ${data.type}`}>
-          <div className='class'>{data.raw}</div>
-        </div>
-      )
-    default:
-      return (
-        <div className={`cell ${data.type}`}>
-          {data.raw}
-        </div>
-      )
-  }
-}
-
-function Time({ name }: { name: string }) {
-  const timeData = timedata.find((t) => t.period === name);
-  return <div className="time">{timeData?.time}</div>;
-}
+import Home from './pages/home/home.tsx'
 
 function App() {
-  //var time = localStorage.getItem('time')
-  var time = sample as Period[]
-
   return (
+    <>
       <section className="app-shell">
-        <div className="app-content">
-          <div className="timetable">
-            <div />
-            <div className="header">月</div>
-            <div className="header">火</div>
-            <div className="header">水</div>
-            <div className="header">木</div>
-            <div className="header">金</div>
-            {time.map((period) => (
-              
-              <>
-                <div className="period">{period.name}
-                  <Time name={period.name} />
-                </div>
-                {period.cells.map((cell) => (
-                  <Cell data={cell} />
-                ))}
-              </>
-            ))}
-          </div>
-        </div>
+        <Home />
       </section>
+      <section className="app-menu">
+        <div className='menu-item'>時間割</div>
+        <div className='menu-item'>設定</div>
+      </section>
+    </>
   )
 }
 
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,84 @@
+import './home.css'
+
+import timedata from '../../assets/time.json'
+import sample from '../..//assets/sample.json'
+
+interface Period {
+    name: string;
+    cells: (ClassCell | OthersCell)[];
+}
+
+interface OthersCell {
+    type: "same" | "empty"
+    raw: "上記と同じ" | "空き"
+}
+
+interface ClassCell {
+    type: "class";
+    subject: string;
+    teacher: string;
+    room?: string;
+};
+
+function Cell({ data }: { data: ClassCell | OthersCell }) {
+    switch (data.type) {
+        case 'class':
+            return (
+                <div className="cell">
+                    <div className='class'>
+                        <div className="class-name">{data.subject}</div>
+                        <div className="teacher">{data.teacher}</div>
+                        <div className="room">{data.room}</div>
+                    </div>
+                </div>
+            )
+        case 'same':
+            return (
+                <div className={`cell ${data.type}`}>
+                    <div className='class'>{data.raw}</div>
+                </div>
+            )
+        default:
+            return (
+                <div className={`cell ${data.type}`}>
+                    {data.raw}
+                </div>
+            )
+    }
+}
+
+const days = ['月', '火', '水', '木', '金'];
+
+function Time({ name }: { name: string }) {
+    const timeData = timedata.find((t) => t.period === name);
+    return <div className="time">{timeData?.time}</div>;
+}
+
+export default function Home() {
+    //var time = localStorage.getItem('time')
+    var time = sample as Period[]
+
+    return (
+        <div className="app-content">
+            <div className="timetable">
+                <div />
+                {
+                    days.map((day) => (
+                        <div className="header">{day}</div>
+                    ))
+                }
+                {
+                    time.map((period) => (
+                        <>
+                            <div className="period">{period.name}
+                                <Time name={period.name} />
+                            </div>
+                            {period.cells.map((cell) => (
+                                <Cell data={cell} />
+                            ))}
+                        </>
+                    ))}
+            </div>
+        </div>
+    )
+}
